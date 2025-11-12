55c733d863b29bfd70416fd008b907592a74aa3d
JuanBejarano-dev <juan.bejarano16.dev@gmail.com>
2025-11-12 16:36:09 -0500

    Agregar .gitignore

--- database.js
+++ database.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '1234',
-    database: 'empleo_corredor'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '1234',
+    database: process.env.DB_NAME || 'empleo_corredor',
+    // Para conexiones en la nube
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
 });
 
 connection.connect((err) => {
