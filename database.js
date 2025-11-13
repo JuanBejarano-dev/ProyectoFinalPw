0f0cf11f12ab7f2bf5c6b0b5badbb9944939c412
JuanBejarano-dev <juan.bejarano16.dev@gmail.com>
2025-11-13 11:55:00 -0500

    Conecto la base de datos de Railway con variables

--- database.js
+++ database.js
@@ -6,6 +6,7 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '1234',
     database: process.env.DB_NAME || 'empleo_corredor',
+    port: process.env.DB_PORT || 3306,
     // Para conexiones en la nube
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined
 });
